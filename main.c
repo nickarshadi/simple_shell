db2770d6f99253eb56bc4d61f8adbd17e5296ae4
Nick <1477@holbertonschool.com>
2020-04-10 12:34:47 -0500

    Add interactive and passive mode

--- main.c
+++ main.c
@@ -12,11 +12,20 @@ int main(int ac, char **av, char **env)
 	size_t n = 0;
 	ssize_t nread = 0;
 	list_t *head = NULL;
+	int printed;
+	int interactive = isatty(STDIN_FILENO);
 
 	listpath(&head, env);
 	while (1)
 	{
-		printf("#cisfun$ ");
+		if (interactive == 1)
+		{
+			printed = printf("#cisfun$ ");
+		} else
+		{
+			printed = printf("#cisfun\n");
+		}
+		wait(&printed);
 		nread = getline(&line, &n, stdin);
 		if (nread != -1)
 		{
@@ -37,6 +46,10 @@ int main(int ac, char **av, char **env)
 		free(line);
 		line = NULL;
 		n = 0;
+		if (interactive != 1)
+		{
+			return (0);
+		}
 	}
 	(void)ac;
 	(void)av;
