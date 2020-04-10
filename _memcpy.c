b0a0052d92432ef51d384b94091774c97bf320a9
gorgyboy <jlrogb@gmail.com>
2020-04-10 10:05:32 -0500

    Change while to for and fix comments

--- _memcpy.c
+++ _memcpy.c
@@ -4,8 +4,7 @@
 #include "shell.h"
 
 /**
- * _memcpy - Copy n bytes from memory area src to \
- memory area dest
+ * _memcpy - Copy n bytes from memory area src to memory area dest
  * @dest: destination
  * @src: source
  * @n: strlen
@@ -13,12 +12,11 @@
  */
 char *_memcpy(char *dest, const char *src, unsigned int n)
 {
-	unsigned int i = 0;
+	unsigned int i;
 
-	while (i < n)
+	for (i = 0; i < n; i++)
 	{
-		*(dest + i) = *(src + i);
-		i++;
+		dest[i] = src[i];
 	}
 	return (dest);
 }
