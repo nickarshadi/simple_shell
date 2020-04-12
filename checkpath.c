ffd5a1ff84598569aff6fbcf5598ac38b680ff5f
Nick <1477@holbertonschool.com>
2020-04-12 00:26:52 -0500

    Solve valgrind errors by having a fix array size

--- checkpath.c
+++ checkpath.c
@@ -12,7 +12,7 @@ char *checkpath(const list_t *head, char *command, char *lcommand)
 {
 	struct stat st;
 	const list_t *node = head;
-	int i, len = 1024;
+	int i, len = 1024 * 8;
 
 	while (node)
 	{
