4077fd6f2af0534fbbc9ea5940cffb8ad3268295
Nick <1477@holbertonschool.com>
2020-04-15 11:14:33 -0500

    Remove illegall functions

--- shell.h
+++ shell.h
@@ -1,10 +1,10 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-#include <fcntl.h>
 #include <stdlib.h>
-#include <stdio.h>
 #include <string.h>
+#include <stdio.h>
+#include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
