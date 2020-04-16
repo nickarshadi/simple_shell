8c32574162f94ad0d0ac6ce6b9ae77a49527d511
Nick <1477@holbertonschool.com>
2020-04-16 17:50:40 -0500

    Remove new changs

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ int main(int ac, char **av, char **env)
 	size_t n = 0;
 	ssize_t nread = 0;
 	list_t *head = NULL;
-	int interactive = isatty(STDIN_FILENO), i = 0, j = 0;
+	int interactive = isatty(STDIN_FILENO);
 
 	signal(SIGINT, sighandle);
 	listpath(&head, env);
@@ -23,7 +23,6 @@ int main(int ac, char **av, char **env)
 		nread = getline(&line, &n, stdin);
 		if (nread != -1)
 		{
-			j++;
 			if (!_strcmp(line, "echo $?\n"))
 			{
 				write(1, "0\n", 2);
@@ -37,8 +36,6 @@ int main(int ac, char **av, char **env)
 			if (interactive != 1)
 				break;
 			_putchar('\n');
-			if (nread == -2)
-				i = j;
 			break;
 		}
 	}
@@ -46,6 +43,6 @@ int main(int ac, char **av, char **env)
 	line = NULL;
 	n = 0;
 	free_list(head);
-	exit(i);
+	exit(0);
 	(void)ac;
 }
